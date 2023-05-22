2d46eddf774cb8f4560c047002af70fc835d1154
이민성 <133635509+minsung-phy@users.noreply.github.com>
2023-05-22 10:46:37 +0900

    Add files via upload

---
+++ 10주차/연습 8.5.py
@@ -0,0 +1,16 @@
+def lotto645():
+    import random
+    numbers = []
+    for _ in range(6):
+        number = random.randrange(1,46)
+        while number in numbers: # 중복되는 숫자 나오지 않게 하려고
+           number = random.randrange(1,46)
+        numbers.append(number)
+    numbers.sort()
+    for number in numbers:
+        if number < 10:
+            print("0"+str(number), end=" ")
+        else:
+            print(number, end=" ")
+    print()
+
